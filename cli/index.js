2bd43ccda2981cffe5b614446be8e21e7229f4b2
Jivansh Sharma <jivansh17@gmail.com>
2021-08-16 14:30:45 +0530

    init: setup cli

---
+++ cli/index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+require("nocamel");
+const axios = require("axios").default;
+
+const axios_instance = argv => {
+    argv.axios = axios.create({
+        baseURL: argv["kiba-url"],
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    return argv;
+};
+
+require("yargs")(process.argv.slice(2))
+    .option("kiba-url", {
+        alias: ["u"],
+        default: "http://127.0.0.1:2000",
+        desc: "Kiba API URL",
+        string: true,
+    })
+    .middleware(axios_instance)
+    .scriptName("kiba")
+    .commandDir("commands")
+    .demandCommand()
+    .help()
+    .wrap(72).argv;
