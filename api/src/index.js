ec6cddf0105a7df401a3df4e02579545aeb2d63e
Jivansh Sharma <jivansh17@gmail.com>
2021-08-20 14:40:30 +0530

    update: improve index using log levels

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,67 @@
 #!/usr/bin/env node
 require("nocamel");
+const Logger = require("logplease");
 const express = require("express");
+const globals = require("./globals");
+const config = require("./config");
+const path = require("path");
+const fs = require("fs/promises");
+const fss = require("fs");
 const body_parser = require("body-parser");
+const runtime = require("./runtime");
 
+const logger = Logger.create("index");
 const app = express();
 
 (async () => {
+    logger.info("Setting loglevel to", config.log_level);
+    Logger.setLogLevel(config.log_level);
+    logger.debug("Ensuring data directories exist");
+
+    Object.values(globals.data_directories).for_each(dir => {
+        let data_path = path.join(config.data_directory, dir);
+
+        logger.debug(`Ensuring ${data_path} exists`);
+
+        if (!fss.exists_sync(data_path)) {
+            logger.info(`${data_path} does not exist.. Creating..`);
+
+            try {
+                fss.mkdir_sync(data_path);
+            } catch (e) {
+                logger.error(`Failed to create ${data_path}: `, e.message);
+            }
+        }
+    });
+
+    logger.info("Loading packages");
+    const pkgdir = path.join(
+        config.data_directory,
+        globals.data_directories.packages
+    );
+
+    const pkglist = await fs.readdir(pkgdir);
+
+    const languages = await Promise.all(
+        pkglist.map(lang => {
+            return fs.readdir(path.join(pkgdir, lang)).then(x => {
+                return x.map(y => path.join(pkgdir, lang, y));
+            });
+        })
+    );
+
+    const installed_languages = languages
+        .flat()
+        .filter(pkg =>
+            fss.exists_sync(path.join(pkg, globals.pkg_installed_file))
+        );
+
+    installed_languages.for_each(pkg => runtime.load_package(pkg));
+
+    logger.info("Starting API Server");
+    logger.debug("Constructing Express App");
+    logger.debug("Registering Middleware");
+
     app.use(body_parser.urlencoded({ extended: true }));
     app.use(body_parser.json());
 
@@ -15,6 +71,8 @@ const app = express();
         });
     });
 
+    logger.debug("Registering Routes");
+
     const kiba_api = require("./api/kiba");
     app.use("/api", kiba_api);
 
@@ -22,7 +80,10 @@ const app = express();
         return res.status(404).send({ message: "Not Found" });
     });
 
-    app.listen(2000, () => {
-        console.log("API server started on port 2000");
+    logger.debug("Calling app.listen");
+    const [address, port] = config.bind_address.split(":");
+
+    app.listen(port, address, () => {
+        logger.info("API server started on", config.bind_address);
     });
 })();
